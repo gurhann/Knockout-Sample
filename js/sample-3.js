638aadcd6652705a1f5325bac446884eb4d23576
Gürhan <gurhan.kucuk@yandex.com>
2016-03-27 13:02:01 +0300

    open mail

--- js/sample-3.js
+++ js/sample-3.js
@@ -3,13 +3,20 @@ function WebmailViewModel() {
   self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
   self.chosenFolderId = ko.observable();
   self.chosenFolderData = ko.observable();
+  self.chosenMailData = ko.observable();
   self.goToFolder = function(folder) {
     self.chosenFolderId(folder);
+    self.chosenMailData(null);
     $.get("mail/"+folder+".json", {}, self.chosenFolderData, "json");
   };
   self.goToFolder('Inbox');
   self.control = function() {
     console.log(self.choosenFolderData());
+  }
+  self.goToMail = function(mail) {
+    self.chosenFolderId(mail.folder);
+    self.chosenFolderData(null);
+    $.get("mail/"+mail.id+".json", {}, self.chosenMailData, "json");
   }
 };
 ko.applyBindings(new WebmailViewModel());
